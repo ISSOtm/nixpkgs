1128a6a029cbd2ba58c042dc40056f2de83e1562
Eldred Habert <me@eldred.fr>
2026-06-27 00:31:21 +0200

    maintainers: correct a name

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -19861,7 +19861,7 @@
     email = "kuba@kadziolka.net";
     github = "meithecatte";
     githubId = 23580910;
-    name = "Jakub Kądziołka";
+    name = "Maja Kądziołka";
     keys = [ { fingerprint = "E576 BFB2 CF6E B13D F571  33B9 E315 A758 4613 1564"; } ];
   };
   nielsegberts = {
